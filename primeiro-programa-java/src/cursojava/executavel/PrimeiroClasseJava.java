9a496148e5a08e136a4ca954e7790e48553f1007
diego <diego@DESKTOP-0SLCON5>
2021-05-20 21:09:25 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -0,0 +1,14 @@
+package cursojava.executavel;
+
+public class PrimeiroClasseJava {
+	
+	/*Main é um metado auto executavel em java*/
+	public static void main(String[] args) {
+		
+		
+		/*Escreve texto no console*/
+		System.out.println("Meu primeiro programa em java");
+		
+	}
+
+}

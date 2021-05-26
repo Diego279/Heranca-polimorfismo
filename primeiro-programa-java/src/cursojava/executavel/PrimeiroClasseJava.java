8bd3f746c7a165b9dd7abc93ae8e5d9dac46a219
diego <diego@DESKTOP-0SLCON5>
2021-05-25 23:01:55 -0300

    Utilizando tipo CHAR em um exemplo com if else

--- primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -3,13 +3,22 @@ package cursojava.executavel;
 public class PrimeiroClasseJava {
 
 	public static void main(String[] args) {
-	
-		double nota1 = 90; 	int nota2 = 70;
-		double nota3 = 35;
-		double nota4 = 55;
-		double mediaFinal=(nota1 + nota2 + nota3 + nota4) /4;
 		
-		System.out.println("Media das notas é = " + mediaFinal);
+		char pessoaFeminino = 'F';
+		
+		/*CHAR representa uma letra ou um caractere*/
+		char pessoaFisica = 'j';
+		
+		if (pessoaFeminino == 'F') {
+			System.out.println("Mascolino");
+		}else {
+			System.out.println("Mascolino");
+			
+		}
+		
+		
+		
+		
 		
 		
 	}

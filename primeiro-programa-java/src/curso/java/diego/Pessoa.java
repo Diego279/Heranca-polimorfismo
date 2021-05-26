136e23903ae4eda641ccf58ebd20df9dc4eae260
diego <diego@DESKTOP-0SLCON5>
2021-05-25 21:54:20 -0300

    Variaveis locais e Globais

---
+++ primeiro-programa-java/src/curso/java/diego/Pessoa.java
@@ -0,0 +1,57 @@
+package curso.java.diego;
+
+import java.util.Date;
+
+public class Pessoa {
+	private String nome;
+	private String sobrenome;
+	private int idade;
+	private Date dataNscimento;
+	private Date dataCadastro;
+	private int numero;
+	private String cpf;
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public Date getDataNscimento() {
+		return dataNscimento;
+	}
+	public void setDataNscimento(Date dataNscimento) {
+		this.dataNscimento = dataNscimento;
+	}
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	
+}
--- primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -1,14 +1,22 @@
 package cursojava.executavel;
 
 public class PrimeiroClasseJava {
+	/*Variavel Global*/
+	static int maiorIdadeGlobal =30;
 	
 	/*Main é um metado auto executavel em java*/
 	public static void main(String[] args) {
 		
-		
-		/*Escreve texto no console*/
-		System.out.println("Meu primeiro programa em java");
-		
-	}
+		/*Variavel local porque pertence somente a esse metado */	
+	int maiorIdade = 18;
+	System.out.println("Valor da variavel local= " + maiorIdade);
+	metado2();
 
+	}
+public static void metado2() {
+	System.out.println("mostre a variavel global " + maiorIdadeGlobal);
+	
+}
+	
+	
 }

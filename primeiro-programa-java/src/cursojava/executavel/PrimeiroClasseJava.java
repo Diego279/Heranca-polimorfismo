c85d5a4b5dff814b1b0e12c3e7ff006d7361d0cf
diego <diego@DESKTOP-0SLCON5>
2021-05-26 21:40:16 -0300

    Utilizando if else com operadores logicos

--- primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -2,27 +2,21 @@ package cursojava.executavel;
 
 public class PrimeiroClasseJava {
 
-	public static void main(String[] args) {
-		
-		char pessoaFeminino = 'F';
-		
-		/*CHAR representa uma letra ou um caractere*/
-		char pessoaFisica = 'j';
-		
-		if (pessoaFeminino == 'F') {
-			System.out.println("Mascolino");
-		}else {
-			System.out.println("Mascolino");
-			
-		}
-		
-		
-		
-		
-		
-		
-	}
-}
 	
-	
+	//
+	public static void main(String[] args) {	
 
+		int mediaAluno = 70;
+		String nome = "Alexa";
+
+		if (mediaAluno >= 70 && nome.equals("Alex")) {
+			System.out.println("Parabens vc foi aprovado");
+		} else if (mediaAluno < 70) {
+			System.out.println("vc está aprovado");
+		}else {
+		 System.out.println(" Aluno não encontrado");
+		
+		}
+		}
+
+}

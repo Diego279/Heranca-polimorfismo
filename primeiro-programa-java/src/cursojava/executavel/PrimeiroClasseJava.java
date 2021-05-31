6707d4e4d5567030a3d60ed9f6b8262c853f99b5
diegoamancio@live.com <diegoamancio@live.com>
2021-05-31 20:49:28 -0300

    Operadores ternários

--- primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -1,22 +1,27 @@
 package cursojava.executavel;
-
 public class PrimeiroClasseJava {
-
-	
-	//
 	public static void main(String[] args) {	
+	int	nota1 = 30;
+	int	nota2 = 90;
+	int	nota3 = 50;
+	int	nota4 = 50;
+	int media = 0 ;
+	 media = (nota1+nota2+nota3+nota4)/4;
+			/*if (media >=70) {
+		System.out.println("Alno aprovado: " + media);
+	}else  if (media >=40 && media <= 69) {
+		 System.out.println("Aluno em recuperação");
+	 }
+		else {
+	  	System.out.println("Aluno reprovado: " + media);
+	  }*/
+		/* Operadores Ternarios são pra micro validação */
+	String saidaResultado;
+	saidaResultado = media >= 70 ? "Aluno Aprovado" : "Aluno Reprovado" ;
+	System.out.println(saidaResultado);
+	  }
+	
+	
+	}
 
-		int mediaAluno = 70;
-		String nome = "Alexa";
 
-		if (mediaAluno >= 70 && nome.equals("Alex")) {
-			System.out.println("Parabens vc foi aprovado");
-		} else if (mediaAluno < 70) {
-			System.out.println("vc está aprovado");
-		}else {
-		 System.out.println(" Aluno não encontrado");
-		
-		}
-		}
-
-}

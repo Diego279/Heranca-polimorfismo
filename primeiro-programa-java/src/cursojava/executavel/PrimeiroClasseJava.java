4af2d1163292c6ee035842822a83a8b90e3179f2
diegoamancio@live.com <diegoamancio@live.com>
2021-05-31 22:36:29 -0300

    Comando Switch e case

--- primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -4,22 +4,29 @@ public class PrimeiroClasseJava {
 	int	nota1 = 80;
 	int	nota2 = 70;
 	int	nota3 = 65;
-	int	nota4 = 63;
+	int	nota4 = 20;
 	int media = 0;
-	 media = (nota1+nota2+nota3+nota4)/4;
-	 System.out.println("A media é: " + media);
-	 if (media >= 50) {
-		 if (media >= 70) {
-			 System.out.println("Aluno está aprovado direto");
-	    }else {
-	    	System.out.println("Aluno está em recuperação");
-	    }
-	 }else {
-		 System.out.println("A Media é = " + media + " Aluno reprovado direto");
+
+		media = (nota1+nota2+nota3+nota4)/4;
 		
-	 }
+		/*SWITCH CASE: OPERAÇÕES EXTAS*/
+		
+		int dia = 3 ;
+		switch (dia) {
+		case 1:
+			System.out.println("domingo");
+			break;
+		case 2:
+			System.out.println("Segunda-Feira");
+			break;
+		case 3:
+			System.out.println("Terça-Feira");
+			break;
+		default: System.out.println("Outro dia");
+			break;
+		}
 	  }
-	
+		
 	
 	}
 

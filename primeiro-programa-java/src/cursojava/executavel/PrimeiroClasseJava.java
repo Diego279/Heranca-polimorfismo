8f307d2d228e75b9c5758efbfd17f97b2349d9d0
diegoamancio@live.com <diegoamancio@live.com>
2021-06-01 20:46:43 -0300

    Repetição For com Break

--- primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -4,15 +4,11 @@ public class PrimeiroClasseJava {
 	
 /*Mein é um metado auto executavel em java*/
 	public static void main(String[] args) {
-		/* Estrutura de repetição FOR com Break*/
-/* inicia o numero com 0, entra na verificação da condição se e menor que 10
- caso não ele incrementa 1 e esse siclo ocorrera ate a condição ser atendida*/
-		
-		/*quando utilizando o break no final da condição informa para parar pois a 
-		 condiçao foi aceita*/
+		/* Estrutura de repetição FOR com Break (Parada)*/
+
 	for (int numero = 0; numero <= 10; numero ++) {
 		if (numero == 3) {
-		System.out.println("Oba encontrei o numer 3");
+		System.out.println("Oba encontrei o numer 3"); 
 		break;
 	    }
 	  }

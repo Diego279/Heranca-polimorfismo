127d241e97698e47f4376b718141fd27b2cd43de
diegoamancio@live.com <diegoamancio@live.com>
2021-05-31 21:58:48 -0300

    Operações lógicas aninhadas, e uma condição dentro de outra condição

--- primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -1,23 +1,23 @@
 package cursojava.executavel;
 public class PrimeiroClasseJava {
 	public static void main(String[] args) {	
-	int	nota1 = 0;
-	int	nota2 = 100;
-	int	nota3 = 100;
-	int	nota4 = 100;
-	int media = 100 ;
+	int	nota1 = 80;
+	int	nota2 = 70;
+	int	nota3 = 65;
+	int	nota4 = 63;
+	int media = 0;
 	 media = (nota1+nota2+nota3+nota4)/4;
-			/*if (media >=70) {
-		System.out.println("Alno aprovado: " + media);
-	}else  if (media >=40 && media <= 69) {
-		 System.out.println("Aluno em recuperação");
+	 System.out.println("A media é: " + media);
+	 if (media >= 50) {
+		 if (media >= 70) {
+			 System.out.println("Aluno está aprovado direto");
+	    }else {
+	    	System.out.println("Aluno está em recuperação");
+	    }
+	 }else {
+		 System.out.println("A Media é = " + media + " Aluno reprovado direto");
+		
 	 }
-		else {
-	  	System.out.println("Aluno reprovado: " + media);
-	  }*/
-		/* Operadores Ternarios são pra micro validação */
-String saidaResultado = media >= 70 ? "Aluno Aprovado" : (media >=40 && media <= 69) ? "Aluno em recuperação" : "Aluno esta em recuperação";
-	System.out.println(saidaResultado);
 	  }
 	
 	

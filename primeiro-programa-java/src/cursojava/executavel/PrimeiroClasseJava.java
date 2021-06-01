131f2a31f87910077338e3d9c15b057daf7a6ef7
diegoamancio@live.com <diegoamancio@live.com>
2021-05-31 22:50:50 -0300

    Operadores Relacionais ==,!=,<,>,<=,>=

--- primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -1,33 +1,15 @@
 package cursojava.executavel;
 public class PrimeiroClasseJava {
 	public static void main(String[] args) {	
-	int	nota1 = 80;
-	int	nota2 = 70;
-	int	nota3 = 65;
-	int	nota4 = 20;
-	int media = 0;
-
-		media = (nota1+nota2+nota3+nota4)/4;
+	int	nota1 = 90;
+	int	nota2 = 90;
+if (nota1 != nota2) {
+	System.out.println("As são difentes ");
+      }else {
+    	  System.out.println("As notas são diferentes");
+      }
+   }
 		
-		/*SWITCH CASE: OPERAÇÕES EXTAS*/
-		
-		int dia = 3 ;
-		switch (dia) {
-		case 1:
-			System.out.println("domingo");
-			break;
-		case 2:
-			System.out.println("Segunda-Feira");
-			break;
-		case 3:
-			System.out.println("Terça-Feira");
-			break;
-		default: System.out.println("Outro dia");
-			break;
-		}
-	  }
-		
-	
-	}
+}
 
 

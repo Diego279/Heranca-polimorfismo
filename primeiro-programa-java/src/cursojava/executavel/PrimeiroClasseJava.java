2426196e4fe00d4d8cb87fde855eaa9e448c9207
diegoamancio@live.com <diegoamancio@live.com>
2021-06-01 20:02:42 -0300

    Estrutura While e Do While

--- primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -1,15 +1,24 @@
 package cursojava.executavel;
+
 public class PrimeiroClasseJava {
-	public static void main(String[] args) {	
-	int	nota1 = 90;
-	int	nota2 = 90;
-if (nota1 != nota2) {
-	System.out.println("As são difentes ");
-      }else {
-    	  System.out.println("As notas são diferentes");
-      }
-   }
+	public static void main(String[] args) {
+	/*Estrutura de repetição while */	
+	
+		int numero = 0;
+	while (numero <= 10) {
 		
+		System.out.println("O numero atual é : " + numero);
+		numero ++;
+		/*----------------------------------------------------*/
+		
+		int numero2 =0;
+		do {/*primeiro executa e depois verifica*/
+			System.out.println("O numero atual é :" + numero2);
+			numero2 ++;
+		} while (numero2 <= 60);
+		}	
+		
+	}
+	
+
 }
-
-

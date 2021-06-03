61404a489e84aa6cb69888e5ea5cfdbd84fc99f6
diegoamancio@live.com <diegoamancio@live.com>
2021-06-03 11:05:28 -0300

    Exemplo de entrada e saida de dados atravez de Dialog

--- primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -5,21 +5,36 @@ import javax.swing.JOptionPane;
 public class PrimeiroClasseJava {
 	public static void main(String[] args) {
 
-		
+
 	/*ShowInput mostra uma entrada de dados tipo dialogo na tela*/
 	/*Essa função e da biblioteca Javax que foi importada */	
 	String carros = JOptionPane.showInputDialog("Infome a quantidade decarros");
     String pessoas = JOptionPane.showInputDialog("Infome a quantidade de pessoas");
 
 	double carroNumero = Double.parseDouble(carros);
+	
 	double pessoaNumero = Double.parseDouble(pessoas);
 	/*Foi Criado uma variavel convertendo para o tipo int */
-		int divisao = (int)(carroNumero / pessoaNumero);
+		
+	int divisao = (int)(carroNumero / pessoaNumero);
 		/*Criado variavel resto onde resto recebe o resto da divisão */
+		
 		double resto = carroNumero % pessoaNumero;
-		JOptionPane.showMessageDialog(null,"Divisão para as pessoas deu " + divisao + " carros para cada, e sobrou " + resto + " carros");
+		
+		int resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o resultado da divisão?");
+		
+		if (resposta == 0) {
+			JOptionPane.showMessageDialog(null,"Divisão para as pessoas deu " + divisao);	
+		}else {
+			System.out.println("Não quis ver o resultado");
+		}
+		
+		resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o RESTO da divisão");
+		if (resposta == 0) {
+			JOptionPane.showMessageDialog(null, "O resto da divisão é " + resto);
+		}
+		
+		
 
 	}	
 	}
-	
-

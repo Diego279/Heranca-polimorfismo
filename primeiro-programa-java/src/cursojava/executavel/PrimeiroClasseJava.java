24534db14517cd624f3e4c784ceda602c20bdcac
diegoamancio@live.com <diegoamancio@live.com>
2021-06-02 21:22:16 -0300

    Criando entrada e saida de dados na tela

--- primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -1,13 +1,24 @@
 package cursojava.executavel;
 
+import javax.swing.JOptionPane;
+
 public class PrimeiroClasseJava {
 	public static void main(String[] args) {
-	/*Estrutura resto de uma divisão com MOD */	
 
-	double carro = 9;
-	double pessoa = 2;
-	double resto = carro % pessoa;
-	System.out.println("Sobraram  exatamente: " + resto + " carro.!!!");
+		
+	/*ShowInput mostra uma entrada de dados tipo dialogo na tela*/
+	/*Essa função e da biblioteca Javax que foi importada */	
+	String carros = JOptionPane.showInputDialog("Infome a quantidade decarros");
+    String pessoas = JOptionPane.showInputDialog("Infome a quantidade de pessoas");
+
+	double carroNumero = Double.parseDouble(carros);
+	double pessoaNumero = Double.parseDouble(pessoas);
+	/*Foi Criado uma variavel convertendo para o tipo int */
+		int divisao = (int)(carroNumero / pessoaNumero);
+		/*Criado variavel resto onde resto recebe o resto da divisão */
+		double resto = carroNumero % pessoaNumero;
+		JOptionPane.showMessageDialog(null,"Divisão para as pessoas deu " + divisao + " carros para cada, e sobrou " + resto + " carros");
+
 	}	
 	}
 	

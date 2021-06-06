c50cc8ba72f61f8ca571b331f31d4782e546ea49
diegoamancio@live.com <diegoamancio@live.com>
2021-06-06 20:02:29 -0300

    Criação da classe e Metodo Construtor

---
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -0,0 +1,28 @@
+package cursojava.classes;
+
+public class Aluno {
+	String nome;
+	int idade;
+	String dataNascimrnto;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;  
+	
+	public Aluno() {
+		// TODO Auto-generated constructor stub
+	}
+public Aluno(String nomePadrao) {
+	nome = nomePadrao;
+}
+
+public Aluno (String nomePadrao, int idadePadrao) {
+nome = nomePadrao;
+idade = idadePadrao;
+} 
+
+
+}
--- primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -1,41 +1,19 @@
 package cursojava.executavel;
 
-import javax.swing.JOptionPane;
+import cursojava.classes.Aluno;
 
 public class PrimeiroClasseJava {
 	/*Main é um metado auto executavel em java*/
 	public static void main(String[] args) {
-
+	Aluno aluno1 = new Aluno();/*Aqui será o João*/
+	
+	Aluno aluno2 = new Aluno();/*Aqui será o Pedro*/
+	
+	Aluno aluno3 = new Aluno(); /*Aqui será o Alex*/
+	
+	Aluno aluno4 = new Aluno("maria");
 		
-	String nota1 = JOptionPane.showInputDialog("Inform a nota 1");
-	String nota2 = JOptionPane.showInputDialog("Inform a nota 2");
-	String nota3 = JOptionPane.showInputDialog("Inform a nota 3");
-	String nota4 = JOptionPane.showInputDialog("Inform a nota 4");
-	
-	double dNota1 = Double.parseDouble(nota1);
-	double dNota2 = Double.parseDouble(nota2);
-	double dNota3 = Double.parseDouble(nota3);
-	double dNota4 = Double.parseDouble(nota4);
-
-	double media = (dNota1 + dNota2 + dNota3 + dNota4) /4;
-	
-	JOptionPane.showConfirmDialog(null, "A media é : " + media);
-
-	
-	/*Média para aprovação é 70*/
-	
-	if (media >=50) {
-		
-		if (media >= 70) {
-			JOptionPane.showMessageDialog(null, "Aluno está aprovado com a média de :" + media);
-		}else {
-			JOptionPane.showMessageDialog(null, "Aluno em recuperação com a media de :" + media);
-		}
-	}else {		
-		JOptionPane.showMessageDialog(null, "Aluno está repravo  :" + media);
-	}
-	
-	
-	}	
+	Aluno aluno5 = new Aluno("José ", 50);/*Construtur com parametro*/
 	}	
 	
+}	

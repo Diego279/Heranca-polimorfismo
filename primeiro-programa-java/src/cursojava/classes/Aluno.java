0733468764bbad1ed08c080367b962cf6edf98ca
diegoamancio@live.com <diegoamancio@live.com>
2021-06-06 20:35:17 -0300

    Declarando os tipos de acesso public ou private

--- primeiro-programa-java/src/cursojava/classes/Aluno.java
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -1,16 +1,16 @@
 package cursojava.classes;
 
 public class Aluno {
-	String nome;
-	int idade;
-	String dataNascimrnto;
-	String registroGeral;
-	String numeroCpf;
-	String nomeMae;
-	String nomePai;
-	String dataMatricula;
-	String nomeEscola;
-	String serieMatriculado;  
+	 public String nome;
+	 public	int idade;
+                   String dataNascimrnto;
+	 String registroGeral;
+	 String numeroCpf;
+	 String nomeMae;
+	 String nomePai;
+	 String dataMatricula;
+	 String nomeEscola;
+	 String serieMatriculado;  
 	
 	public Aluno() {
 		// TODO Auto-generated constructor stub
@@ -19,7 +19,7 @@ public Aluno(String nomePadrao) {
 	nome = nomePadrao;
 }
 
-public Aluno (String nomePadrao, int idadePadrao) {
+public Aluno (String nomePadrao,   int idadePadrao) {
 nome = nomePadrao;
 idade = idadePadrao;
 } 
--- primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -6,14 +6,19 @@ public class PrimeiroClasseJava {
 	/*Main é um metado auto executavel em java*/
 	public static void main(String[] args) {
 	Aluno aluno1 = new Aluno();/*Aqui será o João*/
-	
 	Aluno aluno2 = new Aluno();/*Aqui será o Pedro*/
-	
 	Aluno aluno3 = new Aluno(); /*Aqui será o Alex*/
-	
 	Aluno aluno4 = new Aluno("maria");
-		
 	Aluno aluno5 = new Aluno("José ", 50);/*Construtur com parametro*/
+	
+	aluno1.nome = "João";
+	aluno1.idade = 50;
+			
+	System.out.println("Nome do aluno 1 é: " + aluno1.nome + "e sua idade é " + aluno1.idade);
+	
+	
+	
 	}	
+	
 	
 }	

08408dc4d993411a482edec9a0cb2e52724c177a
diegoamancio@live.com <diegoamancio@live.com>
2021-06-14 23:39:51 -0300

    Associação de classe

--- primeiro-programa-java/src/cursojava/classes/Aluno.java
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -12,27 +12,32 @@ public class Aluno {
 	 private String nomeEscola;
 	 private String serieMatriculado;  
 	 
+	 private Disciplina disciplina = new Disciplina();
 	 
-	 private double nota1;
-	 private double nota2;
-	 private double nota3;
-	 private double nota4;
+	 public void setDisciplina(Disciplina disciplina) {
+		this.disciplina = disciplina;
+	}
+	 
+	 public Disciplina getDisciplina() {
+		return disciplina;
+	}
+	 
 	
 	public Aluno() {
 		// TODO Auto-generated constructor stub
 	}
-public Aluno(String nomePadrao) {
-	nome = nomePadrao;
+	public Aluno(String nomePadrao) {
+		nome = nomePadrao;
 }
 
-public Aluno (String nomePadrao,   int idadePadrao) {
-nome = nomePadrao;
-idade = idadePadrao;
+	public Aluno (String nomePadrao,   int idadePadrao) {
+		nome = nomePadrao;
+		idade = idadePadrao;
 } 
 
-/* Veremos o Metodos SETTERS E GETTERS do Objeto */
-/*SET é para adicionar ou receber dados para os atribustos*/
-/*GET é para resgastar ou obter o valor do atributo */
+	/* Veremos o Metodos SETTERS E GETTERS do Objeto */
+	/*SET é para adicionar ou receber dados para os atribustos*/
+	/*GET é para resgastar ou obter o valor do atributo */
 
 public void setNome(String nome) {
 	this.nome = nome;
@@ -94,35 +99,24 @@ public String getSerieMatriculado() {
 }
 public void setSerieMatriculado(String serieMatriculado) {
 	this.serieMatriculado = serieMatriculado;
-	/*Setter e Getter Variavel notas */
 }
-public double getNota1() {
-	return nota1;
+
+
+
+@Override
+public String toString() {
+	return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral="
+			+ registroGeral + ", numeroCpf=" + numeroCpf + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai
+			+ ", dataMatricula=" + dataMatricula + ", nomeEscola=" + nomeEscola + ", serieMatriculado="
+			+ serieMatriculado + ", disciplina=" + disciplina + "]";
 }
-public void setNota1(double nota1) {
-	this.nota1 = nota1;
-}
-public double getNota2() {
-	return nota2;
-}
-public void setNota2(double nota2) {
-	this.nota2 = nota2;
-}
-public double getNota3() {
-	return nota3;
-}
-public void setNota3(double nota3) {
-	this.nota3 = nota3;
-}
-public double getNota4() {
-	return nota4;
-}
-public void setNota4(double nota4) {
-	this.nota4 = nota4;
-}
+
+
+
 /*Método que retorna a média do aluno*/
 public double getMediaNota() {
- return (nota1 + nota2 + nota3 + nota4) / 4;
+ return (disciplina.getNota1() + disciplina.getNota2() + 
+		 disciplina.getNota3() + disciplina.getNota4()) / 4;
 }
 /*Método que retorna true para aprovado e false para reprovado*/
 public boolean getAlunoAprovado() {
@@ -143,15 +137,6 @@ public String getAlunoAprovado2() {
 	}
 	}
 
-
-@Override
-public String toString() {
-	return "Aluno [nome=" + getNome() + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral="
-			+ registroGeral + ", numeroCpf=" + numeroCpf + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai
-			+ ", dataMatricula=" + dataMatricula + ", nomeEscola=" + nomeEscola + ", serieMatriculado="
-			+ serieMatriculado + ", nota1=" + nota1 + ", nota2=" + nota2 + ", nota3=" + nota3 + ", nota4=" + nota4
-			+ "]";
-}
 @Override
 public int hashCode() {
 	final int prime = 31;
--- primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -1,19 +1,19 @@
+  
 package cursojava.executavel;
 
 import javax.swing.JOptionPane;
 
 import cursojava.classes.Aluno;
-import sun.jvm.hotspot.gc.parallel.PSYoungGen;
+import cursojava.classes.Disciplina;
 
 public class PrimeiroClasseJava {
+	
 	/*Main é um metado auto executavel em java*/
 	public static void main(String[] args) {
 	
 	
-//*=========================================================================*/	
-	
-		/*
-		
+		/*new Aluno() é uma intancia (Criação de Objeto)*/	
+		/* aluno1 é uma referencia para objeto aluno*/
 		
 		String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
 		String idade = JOptionPane.showInputDialog("Qual o sua idade?");
@@ -24,14 +24,23 @@ public class PrimeiroClasseJava {
 		String pai = JOptionPane.showInputDialog("nome do seu pai ?");
 		String matricula = JOptionPane.showInputDialog("Qual sua matrica ?");
 		String serie = JOptionPane.showInputDialog(" Qual sua serie  ?");
+		
+		/*Inserindo dados pelo teclado*/
+		String disciplina1 = JOptionPane.showInputDialog("Disciplina 1?");
 		String note1 = JOptionPane.showInputDialog("Nota 1?");
+		
+		String disciplina2 = JOptionPane.showInputDialog("Disciplina 1?");
 		String note2= JOptionPane.showInputDialog("Nota 2?");
+		
+		String disciplina3 = JOptionPane.showInputDialog("Disciplina 1?");
 		String note3 = JOptionPane.showInputDialog("Nota 3 ?");
+		
+		String disciplina4 = JOptionPane.showInputDialog("Disciplina 1?");
 		String note4= JOptionPane.showInputDialog("Nota 4 ?");
 		
+		/*Criando um Objeto*/
 		Aluno aluno1 =  new Aluno(); 
 				
-
 		aluno1.setNome(nome);
 		aluno1.setIdade(Integer.valueOf(idade));
 		aluno1.setDataNascimento(dataNascimento);
@@ -41,44 +50,25 @@ public class PrimeiroClasseJava {
 		aluno1.setNomePai(pai);
 		aluno1.setDataMatricula(matricula);
 		aluno1.setSerieMatriculado(serie);
-		aluno1.setNota1(Double.parseDouble(note1));
-		aluno1.setNota2(Double.parseDouble(note2));
-		aluno1.setNota3(Double.parseDouble(note3));	
-		aluno1.setNota4(Double.parseDouble(note4));*/
+		
+		/*Setando os dados salvo para as variaveis*/
+		aluno1.getDisciplina().setNota1(Double.parseDouble(note1));
+		aluno1.getDisciplina().setNota2(Double.parseDouble(note2));
+		aluno1.getDisciplina().setNota3(Double.parseDouble(note3));	
+		aluno1.getDisciplina().setNota4(Double.parseDouble(note4));
 	
-		/*
-		System.out.println(aluno1.toString());
+		aluno1.getDisciplina().setDisciplina1(disciplina1);	
+		aluno1.getDisciplina().setDisciplina2(disciplina2);	
+		aluno1.getDisciplina().setDisciplina3(disciplina3);	
+		aluno1.getDisciplina().setDisciplina4(disciplina4);	
+		
+		/*Informando oque vai ser mostrado na tela */
+		System.out.println(aluno1);/*Descrição do Objeto na memoria*/
 		System.out.println("Media do aluno = " + aluno1.getMediaNota());
 		System.out.println("Resultado = " + aluno1.getAlunoAprovado2());
 		System.out.println("");
-		*/
-		
-		/* Equals e hashcode*/
-		
-		
-		Aluno aluno1 = new Aluno();
-		aluno1.setNome("Alex");
-		aluno1.setNumeroCpf("123");
-		
-		Aluno aluno2 = new Aluno();
-		aluno2.setNome("Alex");
-		aluno2.setNumeroCpf("123");
-		
-		if (aluno1.equals(aluno2)) {
-			System.out.println("Aluno são Iguais ");
-		}else {
-			System.out.println("Alunos são Diferente");
-		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+				
 		
 		
 	}	
